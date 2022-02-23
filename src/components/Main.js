e512522897fabd01be707c8e7778656350cdbedd
Anil Varma <varma.rymec@gmail.com>
2022-02-24 02:10:00 +0530

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../components/Main.css'
+
+function Main() {
+
+    <section className='main'>
+
+        <h1>
+
+            hello
+
+        </h1>
+
+
+    </section>
+}
+
+export default Main;
